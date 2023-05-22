f1e392093e3b2c647d48cc76bc89447a43070028
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-05-22 15:16:26 -0600

    Update Container.jsx
    Se agregaron comentarios

--- src/componets/Container.jsx
+++ src/componets/Container.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
-import FromTodo from './/FromTodo';
-import TaskList from './/TaskList';
-
+import FromTodo from './FromTodo';  // Componente para agregar elementos a la lista de tareas
+import TaskList from './TaskList';  // Componente para mostrar la lista de tareas
 
 const Container = () => {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState([]);  // Estado para almacenar la lista de tareas
 
     const addItem = addItem => {
-        setList([...list, addItem])
+        setList([...list, addItem])  // Función para agregar un elemento a la lista de tareas
     };
         
     return (
         <div>
-            <FromTodo addItem={addItem}/>
-            <TaskList list={list} setList={setList}/>
+            <FromTodo addItem={addItem}/>  {/* Componente para agregar nuevos elementos a la lista */}
+            <TaskList list={list} setList={setList}/>  {/* Componente para mostrar la lista de tareas */}
         </div>
     );
 }
